fe059d5d86e66635103a9dc556ae08b7b5fd7eb2
ARUN <aryan.arunachalam@gmail.com>
2026-08-19 06:50:57 +0000

    Add script to retrieve EC2 instance information
    This script uses Boto3 to connect to AWS and retrieve EC2 instance details.

---
+++ aws/day-02/tools/instances.py
@@ -0,0 +1,28 @@
+ cat instances.py 
+import boto3
+
+
+aws = boto3.Session(
+    profile_name="user1",
+    region_name="ap-south-1"
+)
+
+ec2 = aws.client("ec2")
+
+
+def get_ec2_instances():
+    response = ec2.describe_instances()
+
+    instances = []
+
+    for reservation in response["Reservations"]:
+        for instance in reservation["Instances"]:
+            instances.append({
+                "instance_id": instance["InstanceId"],
+                "state": instance["State"]["Name"],
+                "instance_type": instance["InstanceType"],
+                "private_ip": instance.get("PrivateIpAddress"),
+                "public_ip": instance.get("PublicIpAddress")
+            })
+
+    return instances
